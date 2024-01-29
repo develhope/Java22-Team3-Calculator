50ec87a4c179963a41bce32506a21005bb04b941
Bisco <alidero1605@gmail.com>
2024-01-29 11:05:54 +0100

    Funzione riconoscimento pari e dispari

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
-
     public static void main(String[] args) {
-// questo è un xcommento
+//      Test della funzione per il riconoscimento di un numero pari o dispari
+        int value = 5;
+        int seconValue = 6;
+
+        evenOrOdd(value);
+        evenOrOdd(seconValue);
+
     }
 
+    public static void evenOrOdd(int value) {
+        if (value % 2 == 0) {
+            System.out.println(value + " Is even");
+        } else {
+            System.out.println(value + " Is odd");
+        }
+    }
 }
